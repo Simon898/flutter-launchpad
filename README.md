269b8586f0c967ac52d8ed4f31a4804d52804262
Simonh898 <simonh898@gmail.com>
2025-08-01 15:13:48 +0200

    Read me update

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
-# Flutter Launch Pad Starter 🚀
+# Flutter Launch Pad Application
 
-Welcome to **Coding Liquids' Complete Flutter Guide: Build Android, iOS, and Web by Coding Liquids!** 
-This journey kicks off with the **Launch Pad Music App**, an immersive musical experience created 
-by Sagnik Bhattacharya. As part of the "Complete Flutter Guide 2024: Build Android, iOS, and Web apps" course, this app allows you to dive into a world of unique sounds through interactive pads. It's not just an app; it's your first step into the Flutter universe!
+Cross platform app for creating your own music!
 
-## Welcome Aboard the Flutter Launch Pad Project 🚀
 
-Greetings, aspiring Flutter developer! You're about to embark on a significant phase of your Flutter learning adventure with the Launch Pad application. This project isn't just your starting line; it's a foundational piece that will bolster your understanding and hands-on experience with Flutter. Through building and enhancing this app, you'll apply the essential concepts discussed in the course, getting a taste of real-world development challenges and triumphs.
 
-Join us as we explore the realms of app development across Android, iOS, and the web. Let the Launch Pad Music App be your gateway to mastering Flutter, guided by the expertise of Coding Liquids. Ready to start? Let's code away!
 
-### Ready to Launch? 🚀
 
-Enroll in the course today and take your first step into the expansive world of Flutter development. Register now at [flutter.codingliquids.com](https://flutter.codingliquids.com).
-
-### Connect with Sagnik
-
-Want to know more about the creator behind this project? Connect with Sagnik Bhattacharya on [LinkedIn](https://www.linkedin.com/in/sagnik-bhattacharya-916b9463/) and dive deeper into the world of coding and innovation.
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,112 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:audioplayers/audioplayers.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget
+{
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+  // Widget build(BuildContext context)
+}
+
+class _MyAppState extends State<MyApp>
+{
+   @override
+  Widget build(BuildContext context)
+{
+    return  MaterialApp(
+      title: 'Launch Pad',
+      home:SafeArea(
+        child: Scaffold(
+           appBar: AppBar(
+            backgroundColor: Colors.tealAccent,
+            // title: Text('$count'),
+            title: Text('Launch Pad',
+            style: GoogleFonts.orbitron()),
+          ),
+          body:
+          Center(
+            child: Wrap(
+              spacing: 6.0,
+              runSpacing: 6.0,
+              children:[
+                Pad(Color(0xffADCBFC), Color(0xff067CCB), '1.mp3'),
+                Pad(Color.fromARGB(255, 172, 56, 187), Color.fromARGB(255, 181, 125, 203), '2.mp3'),
+                Pad(Color.fromARGB(255, 103, 127, 165), Color.fromARGB(255, 134, 140, 145), '3.mp3'),
+                Pad(Color.fromARGB(255, 213, 230, 80), Color.fromARGB(255, 182, 192, 135), '4.mp3'),
+                Pad(Color(0xffADCBFC), Color(0xff067CCB), '5.mp3'),
+                Pad(Color.fromARGB(255, 172, 56, 187), Color.fromARGB(255, 181, 125, 203), '6.mp3'),
+                Pad(Color.fromARGB(255, 103, 127, 165), Color.fromARGB(255, 134, 140, 145), '7.mp3'),
+                Pad(Color.fromARGB(255, 213, 230, 80), Color.fromARGB(255, 182, 192, 135), '8.mp3'),
+                Pad(Color(0xffADCBFC), Color(0xff067CCB), '9.mp3'),
+                Pad(Color.fromARGB(255, 172, 56, 187), Color.fromARGB(255, 181, 125, 203), '10.mp3'),
+                Pad(Color.fromARGB(255, 103, 127, 165), Color.fromARGB(255, 134, 140, 145), '11.mp3'),
+                Pad(Color.fromARGB(255, 213, 230, 80), Color.fromARGB(255, 182, 192, 135), '12.mp3'),
+                Pad(Color(0xffADCBFC), Color(0xff067CCB), '13.mp3'),
+                Pad(Color.fromARGB(255, 172, 56, 187), Color.fromARGB(255, 181, 125, 203), '14.mp3'),
+                Pad(Color.fromARGB(255, 103, 127, 165), Color.fromARGB(255, 134, 140, 145), '15.mp3'),
+                Pad(Color.fromARGB(255, 213, 230, 80), Color.fromARGB(255, 182, 192, 135), '16.mp3')
+              ]
+              ),
+          ),
+        ),
+      ),);
+}
+}
+
+class Pad extends StatefulWidget {
+  final colorCenter;
+  final colorOutline;
+  final note;
+
+  const Pad(this.colorCenter,this.colorOutline, this.note,{Key? key}) : super(key: key);
+
+  @override
+  State<Pad> createState() => _PadState();
+}
+
+class _PadState extends State<Pad> {
+  late Color _colorCenter;
+  late Color _colorOutline;
+  final player = AudioPlayer();
+  @override
+  void initState() {
+    _colorCenter=widget.colorCenter;
+    _colorOutline = widget.colorOutline;
+    super.initState();
+  }
   Widget build(BuildContext context) {
-    return Container();
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.of(context).size.height;
+    return GestureDetector(
+      onTap: () async{
+        setState(() {
+        _colorCenter = Colors.white;
+        _colorOutline = Colors.white;
+        player.play(AssetSource(widget.note));
+      });
+      await Future.delayed(Duration(microseconds: 500));
+       setState(() {
+        _colorCenter = widget.colorCenter;
+        _colorOutline = widget.colorOutline;
+      });
+      },
+      child: Container(
+        height: deviceHeight/8.2,
+        width: deviceWidth/4.3,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(6.0)),
+          gradient: RadialGradient(colors: [_colorCenter,_colorOutline],
+          radius: 0.5)
+        ),
+      ),
+    );
   }
 }
